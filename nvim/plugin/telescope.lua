5338abffc118365f6fcb50f6a4fd9b3cfbde03b7
lentilus <mail@lentilus.me>
2025-06-04 10:35:46 +0200

    patch lsp_workspace_symbols entry maker

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -6,12 +6,32 @@ vim.g.did_load_telescope_plugin = true
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
+local function entry_maker(entry)
+  local make_entry = require('telescope.make_entry')
+
+  -- Use the default diagnostic entry maker as base
+  local default_maker = make_entry.gen_from_lsp_symbols()
+  local entry_tbl = default_maker(entry)
+
+  -- Just modify the display to be simpler
+  if entry_tbl then
+    entry_tbl.display =
+      string.format('(%s) %s ', vim.fn.fnamemodify(entry.filename or '', ':t'), entry.message or entry.text or '')
+  end
+
+  return entry_tbl
+end
+
 local opts = {
   defaults = {
     layout_strategy = 'vertical',
   },
   pickers = {
     find_files = { hidden = false },
+    lsp_workspace_symbols = {
+      symbol_width = 90,
+      entry_maker = entry_maker,
+    },
   },
 }
 
